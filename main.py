32c4b69002c743c235aa40a66f33419df1af0157
viktor <mychkin.v@gmail.com>
2013-02-03 19:05:46 +0100

    initialized

---
+++ main.py
@@ -0,0 +1,31 @@
+import e_dataset
+import b_emailWrapper as mailWrapper
+
+
+_EMAIL_USER = "isel.dm@gmail.com"
+_PASSWORD="qwertyasdf"
+#_GMAIL_SERVER="imap.gmail.com"
+_GMAIL_SERVER="imap.gmail.com"
+
+
+def populate(mailbox_list =["Inbox"], dateSince = "12-Dec-2012" ):
+   dataset = e_dataset.Dataset()
+   if not dataset.connect(_EMAIL_USER,_PASSWORD,_GMAIL_SERVER): 
+       print "Connection was not established. Check connection settings"
+       return
+   print "connection has been established"
+   #mailbox_list = [ "INBOX", "[Gmail]/Drafts" ]
+ 
+   #dateSince = mailWrapper.get_today()
+   dateSince = "03-Feb-2012"
+   dataset.populateFrom( mailbox_list, mailWrapper.MailAccess.search_DATE_SINCE, dateSince )
+   #dataset.populateFrom( mailbox_list, mailWrapper.MailAccess.search_ALL )
+   # terminate interaction with (IMAP) mail server
+   dataset.disconnect
+
+
+if __name__ == '__main__':
+    populate(["Inbox"], "12-Dec-2012")
+    
+    
+    print "end of line"
